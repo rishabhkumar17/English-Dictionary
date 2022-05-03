f60ecc3f5dac8c11e3f6c40fbfb3f068b7a57f1b
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-03 17:54:45 +0530

    Get close match

--- App.py
+++ App.py
@@ -1,10 +1,14 @@
 import json
+from difflib import get_close_matches
+
 data = json.load(open('./Dataset/data.json'))
 
 def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "The word doesn't exist. Please double check it."
 
