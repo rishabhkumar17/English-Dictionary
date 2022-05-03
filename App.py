5bd59413ee39cc716d59a5e2cb19e1e5eb6579ce
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-03 18:10:56 +0530

    Confirmation from user

--- App.py
+++ App.py
@@ -8,10 +8,16 @@ def translate(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
 
 word = input('Enter word: ')
 
-print(translate(word))
+print(translate(word)) 
