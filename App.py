a64c0d561bf05dcf4c410d44cad81cb31a07ecc5
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-03 18:21:13 +0530

    Optimized the final output

--- App.py
+++ App.py
@@ -20,4 +20,10 @@ def translate(word):
 
 word = input('Enter word: ')
 
-print(translate(word)) 
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
